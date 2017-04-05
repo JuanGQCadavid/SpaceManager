5f83157152520b292172b32272ec2f6d12e97a3e
Juan Cadavid <jquiro12@eafit.edu.co>
2017-04-04 22:45:12 -0500

    Add BDFunctions
    Implementacion de DBFunctions como un intermediario entre la coneccion y
    los metodos comunes de la base de datos.

--- Proyecto/Modelo/DBConection.py
+++ Proyecto/Modelo/DBConection.py
@@ -1,31 +1,73 @@
 # -*- coding: cp1252 -*-
 import MySQLdb
- 
-DB_HOST = 'localhost' 
-DB_USER = 'root' 
-DB_PASS = 'jgoq159' 
-DB_NAME = 'spaceadmind' 
- 
-def run_query(query=''): 
-    datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME] 
- 
-    conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
-    cursor = conn.cursor()         # Crear un cursor 
-    cursor.execute(query)          # Ejecutar una consulta 
- 
-    if query.upper().startswith('SELECT'): 
-        data = cursor.fetchall()   # Traer los resultados de un select 
-    else: 
-        conn.commit()              # Hacer efectiva la escritura de datos 
-        data = None 
- 
-    cursor.close()                 # Cerrar el cursor 
-    conn.close()                   # Cerrar la conexión 
- 
-    return data
 
+
+class DataBaseConection(object):
+    '''
+        Esta clase se encarga de generar
+        y mantener todo lo relacionado con
+        la base de datos.
+        
+    '''
+    def __init__(self):
+ 
+        self.DB_HOST = '127.0.0.1' 
+        self.DB_USER = 'ejemplo' 
+        self.DB_PASS = 'asd123' 
+        self.DB_NAME = 'spaceadmind'
+
+    def initialized(self):
+        self.DB_HOST = '127.0.0.1' 
+        self.DB_USER = 'ejemplo' 
+        self.DB_PASS = 'asd123' 
+        self.DB_NAME = 'spaceadmind'
+        
+
+
+    def run_query(self,query=''):
+        
+        datos = [self.DB_HOST,self.DB_USER, self.DB_PASS, self.DB_NAME] 
+ 
+        conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
+        cursor = conn.cursor()         # Crear un cursor 
+        cursor.execute(query)          # Ejecutar una consulta 
+ 
+        if query.upper().startswith('SELECT'): 
+            data = cursor.fetchall()   # Traer los resultados de un select 
+        else: 
+            conn.commit()              # Hacer efectiva la escritura de datos 
+            data = True 
+ 
+        cursor.close()                 # Cerrar el cursor 
+        conn.close()                   # Cerrar la conexión 
+ 
+        return data
+
+#exampleClass = DataBaseConection()
+#exampleClass.select("*","pais")
+
+'''
+lista = {"pais":("idPais","nombreP")}
+
+query = "insert into pais {} values ('04', 'Alemania Renace ')".format(lista["pais"])
+print query
+
+##exampleClass.run_query(query)
+'''
+
+
+'''
+query = "SELECT * FROM pais" 
+result = run_query(query) 
+print result
+
+query = "insert into pais (idPais,nombreP) values ('03', 'Alemania Mostro ')"
+result = run_query(query)
+print result
 
 query = "SELECT * FROM pais" 
 result = run_query(query) 
 print result
+'''
+
 
---
+++ Proyecto/Modelo/DBFunctions.py
@@ -0,0 +1,31 @@
+# -*- coding: cp1252 -*-
+
+from DBConection import DataBaseConection
+
+class DbFunctions(DataBaseConection):
+    def __init__(self):
+        self.initialized()
+        pass
+
+    def generateMap(self):
+
+        self.Diccionario = {"pais":("idPais","nombreP")}
+        
+    def select(self, atributes ,table):
+        
+        query = "SELECT {} FROM {}".format(atributes,table)
+        return self.run_query(query)
+    
+    def insert(self):
+
+        query = "insert into pais (idPais,nombreP) values ('03', 'Alemania Mostro ')"
+        lista = {"pais":("idPais","nombreP")}
+
+        query = "insert into pais {} values ('04', 'Alemania Renace ')".format(lista["pais"])
+        print self.run_query(query)
+
+    
+
+    
+exampleClass = DbFunctions()
+print exampleClass.select("*","pais")
